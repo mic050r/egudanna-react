459c9e1067ecd5ae3a512448ac30bfdd7d97edab
mic050r <s2216@e-mirim.hs.kr>
2024-06-11 16:15:37 +0900

    🐛 [fix] : 머지 에러 수정

--- src/App.js
+++ src/App.js
@@ -2,27 +2,19 @@ import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-<<<<<<< HEAD
 import VideoPlayer from "../src/components/record/VideoPlayer"; // RecordPage 컴포넌트 import
-=======
-import RecordPage from "./pages/Record"; // RecordPage 컴포넌트 import
-import MainPage from "./pages/Main"
-import SortsPage from "./pages/Sorts"
-import ScreenPage from "./pages/ChallengeSelectionScreen"
->>>>>>> upstream/main
+import MainPage from "./pages/Main";
+import SortsPage from "./pages/Sorts";
+import ScreenPage from "./pages/ChallengeSelectionScreen";
 
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path="/record" element={<VideoPlayer />} />
-=======
-        <Route path="/record" element={<RecordPage />} />
-        <Route path="/main" element={<MainPage/>}/>
-        <Route path="/sorts" element={<SortsPage/>}/>
-        <Route path="/screen" element={<ScreenPage/>}/>
->>>>>>> upstream/main
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/sorts" element={<SortsPage />} />
+        <Route path="/screen" element={<ScreenPage />} />
       </Routes>
     </Router>
   );
