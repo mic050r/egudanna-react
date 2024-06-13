cb4f2f1cc8564208176b879e8f9d7ff02a11b348
leeda06 <slmos33a@gmail.com>
2024-06-13 14:02:53 +0900

    :lipstick: [feat] : 쇼츠화면 - 확인 절차 추가 ( 쓰레기통 )

--- src/components/sorts/ConfirmationDialog.js
+++ src/components/sorts/ConfirmationDialog.js
@@ -3,28 +3,52 @@ import React, { useState } from 'react';
 // 확인 대화상자 컴포넌트
 const ConfirmationDialog = ({ showConfirmation, handleTrashClick }) => {
     const [password, setPassword] = useState('');
+    const [showOverlay, setShowOverlay] = useState(false);
 
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
     };
 
+    const handleConfirmClick = () => {
+        setShowOverlay(true);
+    };
+
+    const handleOverlayConfirmClick = () => {
+        // 비번 확인 
+        handleTrashClick(false);  // handleTrashClick 함수에 비밀번호 전달
+        setShowOverlay(false);       // Overlay를 닫습니다.
+    };
+
+    const handleOverlayCancelClick = () => {
+        setShowOverlay(false);
+    };
+
     return (
         showConfirmation && (
-            <div className="confirmation-div">
-                <p>영상 삭제</p>
-                <div>
-                    <input type="text" placeholder="영상을 업로드했을 때 입력했던 비밀번호를 입력해주세요." value={password} onChange={handlePasswordChange} />
-                    <button onClick={() => {
-                        return (
-                            <div className="confirmation-overlay">
-                                <p>정말 영상을 영구 삭제하시겠습니까?</p>
-                                <div>
-                                    <button>아니오</button>
-                                </div>
-                            </div>)
-                    }}>확인</button>
-                </div>
-            </div>
+            <>
+                {showOverlay ? (
+                    <div className="confirmation-overlay">
+                        <p>정말 영상을 영구 삭제하시겠습니까?</p>
+                        <div>
+                            <button onClick={handleOverlayConfirmClick}>예</button>
+                            <button onClick={handleOverlayCancelClick}>아니오</button>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="confirmation-div">
+                        <p>영상 삭제</p>
+                        <div>
+                            <input
+                                type="password"
+                                placeholder="영상을 업로드했을 때 입력했던 비밀번호를 입력해주세요."
+                                value={password}
+                                onChange={handlePasswordChange}
+                            />
+                            <button onClick={handleConfirmClick}>확인</button>
+                        </div>
+                    </div>
+                )}
+            </>
         )
     );
 };
