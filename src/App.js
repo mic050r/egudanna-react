3cc3f7d09383627c524e4f8a243de6a38b1839a2
mic050r <s2216@e-mirim.hs.kr>
2024-08-08 00:34:48 +0900

    💄 [design] : 카테고리 색 수정

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import VideoPlayer from "../src/components/record/VideoPlayer"; // RecordPage 컴포넌트 import
 import MainPage from "./pages/Main";
 import SortsPage from "./pages/Sorts";
-import ScreenPage from "./pages/screen";
-import IndexnPage from "./pages/index";
+import ScreenPage from "./pages/Screen";
+import IndexPage from "./pages/Index";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/record" element={<VideoPlayer />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/sorts" element={<SortsPage />} />
-        <Route path="/screen" element={<ScreenPage />} />
-        <Route path="/index" element={<IndexnPage />} />
+        <Route path="/record" element={<VideoPlayer />} />{" "}
+        {/* 챌린지 녹화화면 */}
+        <Route path="/sorts" element={<SortsPage />} /> {/* 쇼츠화면 */}
+        <Route path="/screen" element={<ScreenPage />} />{" "}
+        {/* 챌린지 영상 선택 화면 */}
+        <Route path="/" element={<IndexPage />} />
       </Routes>
     </Router>
   );
